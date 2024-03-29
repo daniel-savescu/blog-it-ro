33a990a913f0ccb7877848e5e82ef4d16694592b
daniel-savescu <savescu.daniel.cristian@gmail.com>
2024-03-29 16:08:23 +0200

    added copy code snippets on different pages of the blog

--- js/script.js
+++ js/script.js
@@ -1,9 +1,140 @@
+// Copiaza codurile
+
+// Pagina "Ce este Linux Shell?"
+
+// 1. ls
+function copy_ls_ce_este_linux_shell(){
+    let copyCode = document.getElementById("ls_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_ls_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//2. cd
+
+function copy_cd_ce_este_linux_shell(){
+    let copyCode = document.getElementById("cd_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_cd_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//3. mk dir
+
+function copy_mk_dir_ce_este_linux_shell(){
+    let copyCode = document.getElementById("mk_dir_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_mk_dir_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//4. cp
+
+function copy_cp_ce_este_linux_shell(){
+    let copyCode = document.getElementById("cp_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_cp_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//5. mv
+
+function copy_mv_ce_este_linux_shell(){
+    let copyCode = document.getElementById("mv_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_mv_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//6. rm
+
+
+function copy_rm_ce_este_linux_shell(){
+    let copyCode = document.getElementById("rm_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_rm_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+
+//6. cat
+
+function copy_cat_ce_este_linux_shell(){
+    let copyCode = document.getElementById("cat_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_cat_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//7. grep
+
+function copy_grep_ce_este_linux_shell(){
+    let copyCode = document.getElementById("grep_ce_este_linux_shell");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_grep_ce_este_linux_shell").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+
+//Pagina "Descrie comanda mk dir"
+
 function copy_mk_dir_page_1(){
     let copyCode = document.getElementById("mk_dir_page_1");
  
     copyCode.select();
     document.execCommand("copy");
              
-    document.getElementById("mesaj").innerHTML = "Codul a fost copiat : " + copyCode.value;
+    document.getElementById("mesaj_pagina_mk_dir").innerHTML = "Codul a fost copiat : " + copyCode.value;
 }
+
+//Pagina "Descriere comanda cp"
+
+function copy_cp_page_1(){
+    let copyCode = document.getElementById("cp_page_1");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_cp").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//Pagina "Descriere comanda mv"
+
+function copy_mv_page_1(){
+    let copyCode = document.getElementById("mv_page_1");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_mv").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+//Pagina "Descriere comanda rm"
+
+function copy_rm_page_1(){
+    let copyCode = document.getElementById("rm_page_1");
+ 
+    copyCode.select();
+    document.execCommand("copy");
+             
+    document.getElementById("mesaj_pagina_rm").innerHTML = "Codul a fost copiat : " + copyCode.value;
+}
+
+
     
